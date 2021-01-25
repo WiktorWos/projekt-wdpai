268b6521cc04dccf4b7fee31b7b15ef0592ddc64
WiktorWos <wiktor.wos99@gmail.com>
2021-01-25 10:26:47 +0100

    routing

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('profile', 'DefaultController');
+Router::get('addTicket', 'DefaultController');
+
+Router::run($path);
